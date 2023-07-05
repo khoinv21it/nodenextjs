1b0047b7727adc5321ccd78916e802560c453a18
khoinv21it <khoinguyen010503@gmail.com>
2023-07-05 23:13:40 +0700

    commit

---
+++ constants/dbSettings.js
@@ -0,0 +1,4 @@
+module.exports = {
+  CONNECTION_STRING: 'mongodb+srv://khoinguyen010503:vietkhoi2003@khoi.ccagqzq.mongodb.net/finaltest',
+  DATABASE_NAME: 'finaltest',
+};
---
+++ models/index.js
@@ -0,0 +1,9 @@
+const Category = require('./Category');
+const Product = require('./Product');
+const Supplier = require('./Supplier');
+
+module.exports = {
+  Category,
+  Product,
+  Supplier,
+};
